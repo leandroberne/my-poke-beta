daeedc5ea79ef602342b5da7543f5f44043a4ffc
Leandro Berne <Leandro_Berne@intranet.itresources>
2022-02-02 17:24:46 -0300

    cambios en index del body y pack.jsonssss

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { BodyLayout } from './style';
-import Search from './Search';
 import CardContainer from './CardContainer';
-import Navigator from './Navigator';
 import ShowPokemon from './ShowPokemon';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
